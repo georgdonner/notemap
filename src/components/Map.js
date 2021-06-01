060dee22278accfd5cb5baa4f34a0a8166a050f5
Luca Pohl <pohl.luca@gmx.net>
2021-06-01 08:25:47 +0200

    added function to add Marker on click (currently stored in an array and no addition popup for metadata)

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,43 +1,68 @@
-import React, { useEffect } from 'react'
+import React, {useEffect, useState} from 'react'
 import '../App.css'
-import { MapContainer, TileLayer, Marker, Popup, useMap} from 'react-leaflet'
-import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
+import {MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents} from 'react-leaflet'
+import {GeoSearchControl, OpenStreetMapProvider} from 'leaflet-geosearch';
 
-const SearchField = () => {
-    const provider = new OpenStreetMapProvider();
+const Map = () => {
 
-    const searchControl = new GeoSearchControl({
-        provider: provider,
-        showMarker: false,
-        autoClose: true
-    });
+    const [markers, setMarkers] = useState([]);
 
-    const map = useMap();
-    useEffect(() => {
-        map.addControl(searchControl);
-        return () => map.removeControl(searchControl);
-    }, []);
+    const SearchField = () => {
+        const provider = new OpenStreetMapProvider();
 
-    return null;
-};
+        const searchControl = new GeoSearchControl({
+            provider: provider,
+            showMarker: false,
+            autoClose: true
+        });
 
-function Map() {
+        const map = useMap();
+        useEffect(() => {
+            map.addControl(searchControl);
+            return () => map.removeControl(searchControl);
+        }, []);
+
+        return null;
+    };
+
+    const Markers = () => {
+
+        const map = useMapEvents({
+            click(e) {
+                setNewMarker(e);
+            },
+        });
+
+        return null;
+
+    };
+
+    function setNewMarker(e) {
+        setMarkers([...markers, {id: markers.length, lat: e.latlng.lat, lng: e.latlng.lng, description: "description"}]);
+    }
+
     return (
         <div>
             <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-                <SearchField />
+                <SearchField/>
+                <Markers/>
                 <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                        Test Popup <br/> Easily customizable.
-                    </Popup>
-                </Marker>
+                {markers.map((marker) =>
+                    <Marker key={marker.id} position={[marker.lat, marker.lng]}>
+                        <Popup>
+                            {marker.description}
+                        </Popup>
+                    </Marker>
+                )}
             </MapContainer>
+            {markers.map((marker) =>
+                <li key={marker.id}>{marker.lat} {marker.lng}</li>
+            )}
         </div>
     )
-}
+};
 
 export default Map
