8351b5422f69a961c77ab305471ae80b6ae8c3f9
Luca Pohl <pohl.luca@gmx.net>
2021-06-01 07:10:39 +0200

    added search function (visuals broken)

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,21 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../App.css'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMap} from 'react-leaflet'
+import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
+
+const SearchField = () => {
+    const provider = new OpenStreetMapProvider();
+
+    const searchControl = new GeoSearchControl({
+        provider: provider,
+        showMarker: false,
+        autoClose: true
+    });
+
+    const map = useMap();
+    useEffect(() => {
+        map.addControl(searchControl);
+        return () => map.removeControl(searchControl);
+    }, []);
+
+    return null;
+};
 
 function Map() {
     return (
         <div>
             <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            Test Popup <br /> Easily customizable.
-    </Popup>
-        </Marker>
-      </MapContainer>
+                <SearchField />
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                        Test Popup <br/> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
         </div>
     )
 }
