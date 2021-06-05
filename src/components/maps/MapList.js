42e451be15de1c562d9edef66a686c207634190c
Georg Donner <georgdonner@posteo.de>
2021-06-05 12:00:13 +0200

    Scope maps query to user owned & shared

--- src/components/maps/MapList.js
+++ src/components/maps/MapList.js
@@ -1,4 +1,4 @@
-import { useFirestoreCollectionData, useFirestore } from "reactfire";
+import { useFirestoreCollectionData, useFirestore, useUser } from "reactfire";
 import { Link } from "react-router-dom";
 import tinycolor from "tinycolor2";
 
@@ -8,16 +8,56 @@ const lightenColor = (hex) => {
   return tinycolor(hsl).toHexString();
 };
 
+const MapCard = ({ map }) => (
+  <div key={map.id} className="col-md-6">
+    <div
+      className="card p-4"
+      style={{
+        borderColor: map.color,
+        backgroundColor: lightenColor(map.color),
+      }}
+    >
+      <Link
+        to={`map/${map.id}`}
+        className="h3 stretched-link text-reset text-decoration-none"
+      >
+        {map.name}
+      </Link>
+      {map.description ? (
+        <p className="fs-5 mt-3 mb-0 text-secondary">{map.description}</p>
+      ) : null}
+      <p className="fs-5 mt-3 mb-0">
+        {map.markerCount || 0} Markierung{map.markerCount !== 1 ? "en" : ""}
+      </p>
+    </div>
+  </div>
+);
+
 const MapList = () => {
   const firestore = useFirestore();
+  const { data: user } = useUser();
+
   const mapCollection = firestore.collection("map");
 
-  const { status, data: maps } = useFirestoreCollectionData(mapCollection, {
+  const ownedQuery = mapCollection.where("owner", "==", user.uid);
+  const { data: ownedMaps } = useFirestoreCollectionData(ownedQuery, {
     idField: "id",
     initialData: [],
   });
 
-  const totalMarkers = maps.reduce((a, c) => a + c.markerCount || 0, 0);
+  const sharedQuery = mapCollection.where(
+    "members",
+    "array-contains",
+    user.uid
+  );
+  const { data: sharedMaps } = useFirestoreCollectionData(sharedQuery, {
+    idField: "id",
+    initialData: [],
+  });
+
+  const allMaps = ownedMaps.concat(sharedMaps);
+
+  const totalMarkers = allMaps.reduce((a, c) => a + (c.markerCount || 0), 0);
 
   return (
     <div className="container p-4" style={{ maxWidth: "1000px" }}>
@@ -42,38 +82,15 @@ const MapList = () => {
               einer Karte
             </p>
             <p className="fs-5 mt-3 mb-0">
-              {totalMarkers} Markierung{totalMarkers > 1 ? "en" : ""} von{" "}
-              {maps.length} Karte
-              {maps.length > 1 ? "n" : ""}
+              {totalMarkers} Markierung{totalMarkers !== 1 ? "en" : ""} von{" "}
+              {allMaps.length} Karte
+              {allMaps.length !== 1 ? "n" : ""}
             </p>
           </div>
         </div>
 
-        {maps.map((map) => (
-          <div key={map.id} className="col-md-6">
-            <div
-              className="card p-4"
-              style={{
-                borderColor: map.color,
-                backgroundColor: lightenColor(map.color),
-              }}
-            >
-              <Link
-                to={`map/${map.id}`}
-                className="h3 stretched-link text-reset text-decoration-none"
-              >
-                {map.name}
-              </Link>
-              {map.description ? (
-                <p className="fs-5 mt-3 mb-0 text-secondary">
-                  {map.description}
-                </p>
-              ) : null}
-              <p className="fs-5 mt-3 mb-0">
-                {map.markerCount} Markierung{map.markerCount > 1 ? "en" : ""}
-              </p>
-            </div>
-          </div>
+        {allMaps.map((map) => (
+          <MapCard key={map.id} map={map} />
         ))}
       </div>
     </div>
