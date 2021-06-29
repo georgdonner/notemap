fc6c236d1cc0f7794971b6be7c6e32c95c571cff
Georg Donner <georgdonner@posteo.de>
2021-06-29 17:13:10 +0200

    Update menu icon styles

--- src/components/common/NavbarComp.js
+++ src/components/common/NavbarComp.js
@@ -3,17 +3,20 @@ import { Navbar, Nav } from "react-bootstrap";
 
 const NavbarComp = ({ sidebar, setSidebar }) => {
   function toggleSidebar() {
-    console.log(sidebar);
     setSidebar(!sidebar);
   }
 
   return (
     <div className="d-flex flex-start">
-      <Navbar expand="true" style={{ flex: 1 }} className="ms-3">
+      <Navbar
+        expand="true"
+        style={{ flex: 1, borderBottom: "1px #f2f2f2 solid" }}
+        className="ps-3"
+      >
         <Navbar.Toggle
           aria-controls="basic-navbar-nav"
           onClick={() => toggleSidebar()}
-          style={{ marginRight: "5px" }}
+          className="border-0 px-0 ms-0 me-4"
         />
         <Navbar.Brand href="/">Notemap</Navbar.Brand>
         <Nav className="me-auto">
